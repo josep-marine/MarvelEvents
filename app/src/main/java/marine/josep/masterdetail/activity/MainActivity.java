66635f140af69addeefffaf31a6d3fb926dfb4e5
Josep Marine <josep.marine@gmail.com>
2017-05-02 23:37:30 +0200

    create packages

--- app/src/main/java/marine/josep/masterdetail/activity/MainActivity.java
+++ app/src/main/java/marine/josep/masterdetail/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package marine.josep.masterdetail;
+package marine.josep.masterdetail.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import marine.josep.masterdetail.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/marine/josep/masterdetail/fragment/MainActivityFragment.java
+++ app/src/main/java/marine/josep/masterdetail/fragment/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package marine.josep.masterdetail;
+package marine.josep.masterdetail.fragment;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import marine.josep.masterdetail.R;
 
 /**
  * A placeholder fragment containing a simple view.
